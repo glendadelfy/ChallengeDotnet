575a69c66838be4ca7831a023900c393e79e5130
glenda <glendadelfy20@gmail.com>
2024-11-08 00:21:16 -0300

    Usuario e registro criado com view

---
+++ ProjetoOdontoprev/Migrations/20241006193113_InitialCreate.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProjetoOdontoprev.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "table_dentista",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "NUMBER(10)", nullable: false)
+                        .Annotation("Oracle:Identity", "START WITH 1 INCREMENT BY 1"),
+                    nome_dentista = table.Column<string>(type: "NVARCHAR2(2000)", nullable: false),
+                    email_dentista = table.Column<string>(type: "NVARCHAR2(2000)", nullable: false),
+                    password_dentista = table.Column<string>(type: "NVARCHAR2(2000)", nullable: false),
+                    status_active_dentista = table.Column<bool>(type: "NUMBER(1)", nullable: false),
+                    description_dentista = table.Column<string>(type: "NVARCHAR2(2000)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_table_dentista", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "table_dentista");
+        }
+    }
+}
---
+++ ProjetoOdontoprev/Migrations/20241107213514_MigrationUsuario.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProjetoOdontoprev.Migrations
+{
+    /// <inheritdoc />
+    public partial class MigrationUsuario : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
